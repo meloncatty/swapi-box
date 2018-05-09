18d1fa566ae8fffef9adbd1331087ab46ccd1cb7
meloncatty <k.handel66@gmail.com>
2018-05-08 19:23:54 -0600

    Import Header and SplashPage

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import SplashPage from '../SplashPage/SplashPage'
+import Header from '../Header/Header'
 import './App.sass';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header />
+        <SplashPage />
       </div>
     );
   }
