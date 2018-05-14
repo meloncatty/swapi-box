842b0c8e9daac39b6c02d96c71f2833daa4ec123
meloncatty <k.handel66@gmail.com>
2018-05-13 23:48:47 -0600

    Add header styling Update title icon Update loading animation

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,32 +1,38 @@
 import React from 'react'
+import logo from '../images/logo.svg'
 
 const Header =(props)=> {
-  
+
   return (
     <header>
       <div className='title'>
-        <h1>SWAPI-Box</h1>
-        <button 
-          className='favorites'>
+        <h1><img className='logo' src={logo} alt='Swapi Box, a Star Wars Repository'/></h1>
+        <button
+          className='favorites header-button'>
           View Favorites
-          <span>0</span>
+          <span> 0</span>
         </button>
       </div>
-        <hr></hr>
       <nav>
-        <button onClick={(e) => {
+        <button
+          className='select-category header-button'
+          onClick={(e) => {
             props.peopleInfo()
-            props.updateSelectedState(e) 
+            props.updateSelectedState(e)
           }}
         >People</button>
-        <button onClick={(e) => {
+        <button
+          className='select-category header-button'
+          onClick={(e) => {
             props.planetInfo()
-            props.updateSelectedState(e) 
+            props.updateSelectedState(e)
           }}
         >Planets</button>
-        <button onClick={(e) => {
+        <button
+          className='select-category header-button'
+          onClick={(e) => {
             props.vehicleInfo()
-            props.updateSelectedState(e) 
+            props.updateSelectedState(e)
           }}
         >Vehicles</button>
       </nav>
